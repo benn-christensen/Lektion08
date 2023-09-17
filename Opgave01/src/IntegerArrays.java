e7edfb78fc74e7d3190d9910c71f09091f30919b
Benn Christensen <benn.christensen@wolterskluwer.com>
2023-09-17 14:26:59 +0200

    initial

---
+++ Opgave01/src/IntegerArrays.java
@@ -0,0 +1,11 @@
+public class IntegerArrays {
+    public static void main(String[] args) {
+        int[] integerArray = getIntegerArray();
+    }
+
+    private static int[] getIntegerArray() {
+        int[] integerArray =  { 1095, 12, 9065, 387, 700, 20, 5, 2065, 97654, 103, 789, 50, 1972, 200, 35, 98, 1002 };
+        return integerArray;
+    }
+
+}
